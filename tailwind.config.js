131f5a54a66f7edfa0e1c1e541b5d9da5d0fde4f
Vurak <vurak.of.sulcan@gmail.com>
2021-05-22 04:08:41 +0100

    Added pattern landing removed navigation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './dist/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    
     borderWidth: {
       DEFAULT: '1px',
       '0': '0',
@@ -13,10 +17,32 @@ module.exports = {
       '10': '10px',
       '20': '20px'
     },
-    extend: {},
+    extend: {
+      colors: {
+        'purple': {
+          'dark': '#26004c',
+          'darker': '#190033',
+          'even-darker': '#0c0019'
+        }
+      },
+      height: {
+        'full2': '150%'
+      },
+      margin: {
+        '100': '25rem',
+        '120': '27.5rem',
+        '130': '30rem',
+        '0.25': '0.062rem',
+        '0.2': '0.02rem',
+      },
+      transitionProperty: {
+        'height': 'height'
+      }
+    },
   },
   variants: {
     width: ["responsive", "hover", "focus"],
+    height: ["responsive", "hover", "focus"],
     extend: {
       borderWidth: ['hover', 'focus']
     },
