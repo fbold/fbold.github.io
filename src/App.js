7d94c2fa52cd3268af1b17d2661271d7e3b25f57
Vurak <vurak.of.sulcan@gmail.com>
2021-05-27 01:42:23 +0100

    Seperated out LandingComponent and DropsComponent from App.js to clean up. Removed App.css references

--- src/App.js
+++ src/App.js
@@ -1,119 +1,11 @@
-import './App.css';
 import React from 'react';
-import Rellax from '../node_modules/rellax/rellax.min.js'
 import gamePic from './img/creature2.png'
 import text from './text.js'
 
-function RandInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+import Landing from './LandingComponent.js'
+import Drops from './DropsComponent.js'
 
-function Bar(props) {
-    return (
-        <div
-            key={props.h}
-            className={`rellax flex flex-auto w-full z-0 bg-indigo-${props.c + "00"} ${(props.i === 0 || props.i === 1) ? 'mx-0' : '-mx-px'} rounded${props.loc ? "-" + props.loc : ""}-full`}
-            style={{ 'height': `${props.h}%` }}
-            data-rellax-speed={`${props.s}`}
-            data-rellax-xs-speed={`${props.s * 1.3}`}
-            data-rellax-mobile-speed={`${props.s * 1.3}`}
-            data-rellax-tablet-speed={`${props.s}`}
-            data-rellax-desktop-speed={`${props.s}`}
-        />
-    )
-}
 
-function Bars({ minSpeed, maxSpeed, color, extraHeight = 0, zIndex = 20 }) {
-    var barsTop = []
-    var barsBot = []
-
-    let n = Math.floor(window.innerWidth / 25)
-
-    for (let i = 0; i <= n; i++) {
-        let rs = RandInt(minSpeed, maxSpeed)
-        let rh = 100 * (i / (n)) + extraHeight
-        barsTop.push(<Bar key={i} h={rh} loc="b" s={rs} c={color} i={i / n} />)
-        barsBot.push(<Bar key={i} h={rh} loc="t" s={-rs} c={color} i={i / n} />)
-    }
-
-    barsBot.reverse()
-
-    return (
-        <>
-        <div className={"flex flex-none absolute w-full h-full top-0 items-start z-" + zIndex}>
-            {barsTop}
-        </div>,
-        <div className={"flex flex-none absolute w-full h-full bot-0 items-end overflow-y-hidden z-" + zIndex}>
-            {barsBot}
-        </div>
-    </>)
-}
-
-function Landing() {
-    const [scroll, setScroll] = React.useState(false)
-
-    // Cannot have Rellax creation in here with handleScroll
-    // Had to nest in Bars which makes rellax do weird stuff
-    React.useEffect(() => {
-        new Rellax('.rellax', {
-            breakpoints: [640, 768, 1024]
-        })
-
-        if (window.scrollY > 0) {
-            setScroll(true);
-        }
-
-        const handleScroll = () => {
-            if (window.scrollY > 0) {
-                setScroll(true);
-            } else setScroll(false);
-        }
-
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
-
-    }, [])
-
-    return (
-        <section className="h-screen w-full">
-            <div className="h-full2">
-                <div className="flex sticky top-0 justify-center items-center h-screen w-auto text-center">
-                    {console.log(scroll)}
-                    {scroll ? null :
-                        <svg className="animate-pulse relative h-14 w-full fill-current text-indigo-200 z-30" viewBox="0 0 16 16">
-                            <path fillRule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z" />
-                        </svg>}
-
-                    <header className="absolute w-auto z-10">
-                        <div className="relative text-center object-center content-center items-center p-4">
-                            <span className="text-2xl font-normal text-indigo-200">Hi, I'm <b>Fred</b></span>
-                            <h1 className="text-indigo-400 text-4xl font-sans font-extrabold">welcome to my portfolio</h1>
-                            <div className="relative mt-3"><span className="text-sm font-bold text-indigo-200 animate-pulse">(keep scrolling)</span></div>
-                            <svg className="animate-pulse relative h-5 w-full fill-current text-indigo-200" viewBox="0 0 16 16">
-                                <path fillRule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z" />
-                            </svg>
-                        </div>
-                    </header>
-
-                    <Bars minSpeed={4} maxSpeed={6} color={7} zIndex={0} />
-                    <Bars minSpeed={7} maxSpeed={9} color={6} />
-                    <Bars minSpeed={10} maxSpeed={12} color={5} />
-                    <Bars minSpeed={13} maxSpeed={15} color={4} extraHeight={2} />
-
-                    {/**
-                            <Bars minSpeed={6} maxSpeed={8} color={7} zIndex={0}/>
-                            <Bars minSpeed={9} maxSpeed={12} color={6}/>
-                            <Bars minSpeed={13} maxSpeed={16} color={5}/>
-                            <Bars minSpeed={18} maxSpeed={22} color={4} extraHeight={2}/>
-                            **/}
-
-                </div>
-            </div>
-        </section>
-    )
-}
 
 function Title({ text = '', children }) {
     return (
@@ -138,46 +30,6 @@ function TextElement(props) {
             </p>
         </div>
     )
-}
-
-function Drop(props) {
-    return (
-        <div
-            
-            className={`flex opacity-${props.o} absolute rellax-drops w-6 z-${props.z} bg-indigo-${(props.c) * 100} mx-px md:mx-0.2 rounded-full`}
-            style={{ 'height': `${props.h}rem`, 'top': `${props.t}%`, 'left': `${props.l}%` }}
-            data-rellax-speed={`${props.s}`} />
-    )
-}
-
-function Drops({ o, nDrops, ...props }) {
-    const [drops, setDrops] = React.useState([])
-    //var drops = []
-    let n = Math.floor(nDrops * window.innerWidth / 700)
-
-
-    React.useEffect(() => {
-        // TODO: add event listener for scroll so it only applies rellax class
-        // when past the first scroll amopunt
-        console.log("running useEffect")
-        new Rellax('.rellax-drops')
-
-    }, [drops])
-
-    React.useEffect(() => {
-
-        for (let i = 1; i <= n; i++) {
-            let rh = RandInt(1, 7)
-            let rs = Math.random() * 3
-            let rc = RandInt(6, 9)
-            let rt = Math.round(Math.random() * 100)
-            let rl = Math.random() * 97
-            setDrops(arr => [...arr, <Drop key={i} h={rh} s={rs} c={rc} t={rt} l={rl} z="0" o={o} />])
-            //drops.push(<Drop h={rh} s={rs} c={rc} t={rt} l={rl} z="0" o={o}/>)
-        }
-    }, [n, o])
-
-    return (drops)
 }
 
 function GridElement({ onClick, span = 1, ...props }) {
@@ -212,7 +64,7 @@ function Contents(props) {
 
     function handleClick(test) {
         props.setInfoPanel(!props.infoPanel)
-        console.log(test)
+        
     }
 
     return (
@@ -232,7 +84,7 @@ function Contents(props) {
                     <Title>Projects</Title>
                     <div className="grid grid-cols-1 md:grid-cols-2  xl:grid-cols-3 relative px-2 md:px-20 w-full overflow-auto">
                         <GridElement title="Dog Nights" span={2} onClick={() => { handleClick() }} image={gamePic}>
-                            {text.game.short}                            
+                            {text.game.short}
                         </GridElement>
                         <GridElement title="Dog Nights Game">
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis eum veritatis neque nisi,
@@ -268,26 +120,6 @@ function Contents(props) {
     )
 }
 
-// const OverlayPanel = props => {
-
-//     const handleClick = () => {
-//         props.setInfoPanel(!props.infoPanel)
-//     }
-
-
-//     return (
-//         props.infoPanel ?
-//             <container className="flex justify-center items-center" >
-//                 <div className="fixed mt-auto top-0 h-full w-full bg-indigo-even-darker2 opacity-70 z-40" onClick={handleClick} />
-//                 <div className="fixed m-auto top-1/4 w-2/3 bg-indigo-even-darker z-40 o-100" />
-//             </container>
-//             : null
-
-
-//     )
-
-// }
-
 const MoreInfo = props => {
 
     return (
@@ -299,6 +131,12 @@ const MoreInfo = props => {
     )
 
 }
+
+//========
+// TODO:  Use React context to store what text element we are currently on y setting it on click on one of the Grid elements or whatever
+// TODO:  That way you can keep track without large strign everywhere. Hiopefully this is the solution you are looking for. Learn Vim.
+//========
+
 
 const TestHide = props => {
     const handleClick = () => {
@@ -323,6 +161,7 @@ const TestHide = props => {
             </>
     )
 }
+
 
 function App() {
     const [infoPanel, setInfoPanel] = React.useState(false)
---
+++ src/DropsComponent.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import Rellax from '../node_modules/rellax/rellax.min.js'
+
+
+function RandInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function Drop(props) {
+    return (
+        <div
+            
+            className={`flex opacity-${props.o} absolute rellax-drops w-6 z-${props.z} bg-indigo-${(props.c) * 100} mx-px md:mx-0.2 rounded-full`}
+            style={{ 'height': `${props.h}rem`, 'top': `${props.t}%`, 'left': `${props.l}%` }}
+            data-rellax-speed={`${props.s}`} />
+    )
+}
+
+function Drops({ o, nDrops, ...props }) {
+    const [drops, setDrops] = React.useState([])
+    //var drops = []
+    let n = Math.floor(nDrops * window.innerWidth / 700)
+
+
+    React.useEffect(() => {
+        // TODO: add event listener for scroll so it only applies rellax class
+        // when past the first scroll amopunt
+        console.log("running useEffect")
+        new Rellax('.rellax-drops')
+
+    }, [drops])
+
+    React.useEffect(() => {
+
+        for (let i = 1; i <= n; i++) {
+            let rh = RandInt(1, 7)
+            let rs = Math.random() * 3
+            let rc = RandInt(6, 9)
+            let rt = Math.round(Math.random() * 100)
+            let rl = Math.random() * 97
+            setDrops(arr => [...arr, <Drop key={i} h={rh} s={rs} c={rc} t={rt} l={rl} z="0" o={o} />])
+            //drops.push(<Drop h={rh} s={rs} c={rc} t={rt} l={rl} z="0" o={o}/>)
+        }
+    }, [n, o])
+
+    return (drops)
+}
+
+export default Drops
